a3ecc66fb680238eadadc305b4afd8b6219d6ae7
ElephantT <33278581+ElephantT@users.noreply.github.com>
2021-05-15 19:52:18 +0300

    Update my_Netflix_bot.py

--- my_Netflix_bot.py
+++ my_Netflix_bot.py
@@ -249,7 +249,7 @@ if __name__ == "__main__":
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
+        #on_shutdown=on_shutdown,
         skip_updates=False,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
